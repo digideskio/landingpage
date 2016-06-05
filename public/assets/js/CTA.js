d05807e7ba55637bf7040cdda0a8176daa14f280
FrancescoSTL <francescosapolizzi@gmail.com>
2016-06-04 17:35:18 -0700

    Copy update, fixing typed.js.

--- public/assets/js/CTA.js
+++ public/assets/js/CTA.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	var waypoint = new Waypoint({
-	  	element: document.getElementById("download"),
+	  	element: document.getElementById("secondFeature"),
 	  	handler: function() {
 			$(function(){
 		    	$(".ctaHeader").typed({
-		        	strings: ["We can't do this alone."],
-		        	typeSpeed: 10
+		        	strings: ["We can't do this alone.", "Join us."],
+		        	typeSpeed: 50
 		    	});
 			});
 	  	},
